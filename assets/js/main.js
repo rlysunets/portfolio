8098d00874832e78cf27f8ab44ff9b0fdc4559b8
Roman-Lysunetss <rlysunets@gmail.com>
2022-08-26 17:30:51 +0300

    added section about, added js file(scroll & click events)

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const arrow = document.querySelector(".intro_wrap_arrow");
+const header = document.querySelector(".header");
+const introHeight = document.querySelector(".intro").clientHeight;
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > introHeight) {
+        header.classList.add("header-small")
+    } else {
+        header.classList.remove("header-small")
+    }
+})
+
+arrow.addEventListener("click", () => {
+    const screenHeight = window.innerHeight;
+    const headerHeigh = header.clientHeight;
+    window.scrollTo({top: screenHeight-headerHeigh, behavior: "smooth"});
+}) 
